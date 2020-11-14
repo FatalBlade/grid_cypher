25d615cf6cefe966131441e50f35705b74c1ec38
Alexander <Swordm.1993@gmail.com>
2020-11-14 01:23:34 +0000

    Created index var, Returned indexes of identified char in create_page

--- Page.py
+++ Page.py
@@ -4,22 +4,24 @@ import random
 import string
 import settings
 
-char = [] # List of usable digits
 char_test = ''
+index = []
 
 def create_page(bytes):
     """ This function creates the page  """
     page = ''.join(random.choice(string.printable) for i in range(bytes)) 
     char_count = 0 # Used to determine total number of usable characters
     global char_test
+    global index
     print(page)
-    for letter in page:
+   
+    for position, letter in enumerate(page):
         if letter in ['1', '2', '3', '4', '5']:
-            char.append(letter) # Adds digit to usuable list
+            index.append(position)
             char_test = char_test + str(letter)
             char_count = char_count + 1 # Increments character count 
             if settings.debug:
                 print('Found ' + letter) # Prints which number was found
                 print(char_count) # Prints current character count
-    print(char)
-    print(char_test)
+    if settings.debug: print(char_test)
+    return (index, char_test, page)
