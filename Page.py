56a65fbaed276297b870f818ff7b9afb2b675882
Alexander <Swordm.1993@gmail.com>
2020-11-14 02:59:50 +0000

    Changed global variables into  local variables.

--- Page.py
+++ Page.py
@@ -4,24 +4,23 @@ import random
 import string
 import settings
 
-char_test = ''
-index = []
+
 
 def create_page(bytes):
     """ This function creates the page  """
     page = ''.join(random.choice(string.printable) for i in range(bytes)) 
     char_count = 0 # Used to determine total number of usable characters
-    global char_test
-    global index
+    char_values = '' # string of returend digets between 1 and 5
+    index = [] # List of indexes of usable characters
     print(page)
    
     for position, letter in enumerate(page):
         if letter in ['1', '2', '3', '4', '5']:
-            index.append(position)
-            char_test = char_test + str(letter)
+            index.append(position) # Pulls indexes of char for later use
+            char_values = char_values + str(letter)
             char_count = char_count + 1 # Increments character count 
             if settings.debug:
                 print('Found ' + letter) # Prints which number was found
                 print(char_count) # Prints current character count
-    if settings.debug: print(char_test)
-    return (index, char_test, page)
+    if settings.debug: print(char_values)
+    return (index, char_values, page)
